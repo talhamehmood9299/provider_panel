6580bdd0bb238b96205b8cdc740385d564a960e9
Talha Mehmood <amaanullah.azz@gmail.com>
2024-07-23 20:50:46 +0500

    added persistant

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authReducer from "./reducers/authReducer";
 import providersReducer from "./reducers/providersReducer";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    provider: providersReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  provider: providersReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/FLUSH",
+          "persist/PAUSE",
+          "persist/PERSIST",
+          "persist/PURGE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
