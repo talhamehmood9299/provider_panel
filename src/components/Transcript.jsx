92da5fea4e969a60e40609d97dcd838b65c8f80b
Talha Mehmood <amaanullah.azz@gmail.com>
2024-07-26 20:16:00 +0500

    fix merge conflicts

--- src/components/Transcript.jsx
+++ src/components/Transcript.jsx
@@ -1,14 +1,15 @@
 const Transcript = () => {
   return (
-    
     <div className="flex flex-col gap-6 w-full">
-    <h2 className="text-xl font-bold text-[#1E328F]">Dictation Note</h2>
-   
-    <div className="h-[40%] bg-white rounded-xl border p-4">
-      Comment here...
+      <h2 className="text-xl font-bold text-[#1E328F]">Transcript</h2>
+
+      <div className="h-[40%] bg-white rounded-xl border p-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, facere!
+        Minus accusamus voluptates at delectus maxime soluta neque nemo, beatae,
+        doloribus fugit temporibus ipsum expedita doloremque dicta molestias,
+        tempore harum?
+      </div>
     </div>
-    
-  </div>
   );
 };
 
