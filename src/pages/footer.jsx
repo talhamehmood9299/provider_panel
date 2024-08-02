6f7793c4e07e0b63b548866582a63773c3d963bc
Talha Mehmood <amaanullah.azz@gmail.com>
2024-08-02 21:38:45 +0500

    fix conflicts

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -8,16 +8,20 @@ const Footer = () => {
   );
   return (
     <div className="relative w-full z-[1000]">
-      <div className="absolute bottom-0 w-full py-3 bg-blue-900 text-white px-24 flex justify-between items-center">
-        <div className="flex items-center space-x-2">
-          <IoLocationOutline size={32} />
-          <div className="w-[60%]">
-            <p>{selectedAddress}</p>
+      <div className="absolute bottom-0 w-full py-3 bg-blue-900 text-white px-4 md:px-24 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <IoLocationOutline className="w-6 h-6 md:h-8" />
+          <div className="w-full md:w-[60%]">
+            <p className="text-xs md:text-base text-center md:text-left">
+              {selectedAddress}
+            </p>
           </div>
         </div>
-        <div className="flex items-center space-x-4">
-          <FaPhoneVolume size={32} />
-          <p>609-890-1050</p>
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <FaPhoneVolume className="w-6 h-6 md:w-8 md:h-8" />
+          <p className="text-xs md:text-base text-center md:text-left">
+            609-890-1050
+          </p>
         </div>
       </div>
     </div>
